d6a3dff7105ac800eee60ec1f684ef67b5e262cb
balueva <81beu@mail.ru>
2021-10-25 15:47:49 +0700

    lesson 03

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import './App.scss';
+import { AUTHOR_ROBOT } from './const';
+
 import Message from './Message.jsx';
 import MessageForm from './MessageForm.jsx';
 import ShowMessages from './ShowMessages.jsx';
+import ChatList from './ChatList.jsx';
 
 function App() {
   const [messageList, setMessageList] = useState([]);
+  const [chatList, setChatList] = useState([
+    { id: 1, caption: 'Школа 2В', statusStr: 'Класс 2В', isGroup: true, avatar: '' },
+    { id: 2, caption: 'Школа 2В родители', statusStr: 'Класс 2В только родители', isGroup: true, avatar: 'panda.jpg' },
+    { id: 3, caption: 'Liberty school', statusStr: 'Liberty school Famili&Friends-2', isGroup: true, avatar: '' },
+    { id: 4, caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
+    { id: 5, caption: 'Лиза', statusStr: '', isGroup: false, avatar: 'pikachu.jpg' },
+    { id: 6, caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' },
+    { id: 7, caption: 'Ольга', statusStr: '', isGroup: false, avatar: '' },
+    { id: 8, caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
+    { id: 9, caption: 'Лиза', statusStr: '', isGroup: false, avatar: '' },
+    { id: 10, caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' },
+    { id: 11, caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
+    { id: 12, caption: 'Лиза', statusStr: '', isGroup: false, avatar: '' },
+    { id: 13, caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' },
+    { id: 14, caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
+    { id: 15, caption: 'Лиза', statusStr: '', isGroup: false, avatar: '' },
+    { id: 16, caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' }
+  ]);
 
   const addMessage = (author, text) => {
     const newMessageList = [...messageList, { id: uuidv4(), author: author, text: text, date: new Date() }];
@@ -16,8 +37,8 @@ function App() {
 
   useEffect(() => {
     const robotAnswer = setTimeout(() => {
-      if (messageList.length > 0 && messageList[messageList.length - 1].author !== 'Robot')
-        addMessage('Robot', 'Здравствуйте! Это Робот.')
+      if (messageList.length > 0 && messageList[messageList.length - 1].author !== AUTHOR_ROBOT)
+        addMessage(AUTHOR_ROBOT, 'Здравствуйте! Это Робот.')
     }, 1500);
 
     return () => { clearTimeout(robotAnswer) };
@@ -25,9 +46,13 @@ function App() {
 
   return (
     <div className="App">
-      <div class="authors">Собеседники, группы</div>
+      <div class="chats">
+        <div class="menu">Меню списка чатов</div>
+        <ChatList chatList={chatList} />
+        <div class="menu">Футер</div>
+      </div>
       <div class="messages">
-        <div class="messages__menu">Меню</div>
+        <div class="menu">Меню активного чата</div>
         <ShowMessages messageList={messageList} />
         <MessageForm addMessage={addMessage} />
       </div>
--- src/MessageForm.jsx
+++ src/MessageForm.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.scss';
+import { AUTHOR_ME } from './const';
+
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SendIcon from '@mui/icons-material/Send';
 
 function MessageForm({ addMessage }) {
 
@@ -8,18 +13,33 @@ function MessageForm({ addMessage }) {
     setTextareaValue(event.target.value);
   };
 
+  const refTextarea = useRef(undefined);
+
+  //фокус при первой отрисовке, если убрать autoFocus у TextField
+  /*
+  useEffect(() => {
+    refTextarea.current?.focus();
+  }, []);
+*/
   const addUserMessage = (event) => {
     event.preventDefault();
     if (textareaValue !== '') {
-      addMessage('XXX', textareaValue);
+      addMessage(AUTHOR_ME, textareaValue);
       setTextareaValue('');
     }
+    refTextarea.current.focus();
   };
 
   return (
     <form class="sendmessage" action="#" onSubmit={addUserMessage}>
-      <textarea value={textareaValue} onChange={onTextareaChange} cols="30" rows="5"></textarea>
-      <button type="submit">Отправить</button>
+      <TextField
+        id="outlined-multiline-flexible" multiline rows={3} variant="standard" fullWidth
+        color="success"
+        value={textareaValue} onChange={onTextareaChange} inputRef={refTextarea} autoFocus
+      />
+      <IconButton color="primary" type="submit" variant='outlined' color="success">
+        <SendIcon />
+      </IconButton>
     </form>
   )
 }
